6283b95dd81e48027a38814058ea7329ccb51991
Thomas Nguyen <ttn@ou.edu>
2023-01-16 09:56:27 -0600

    Initial commit

---
+++ components/Rewards/rewards.tsx
@@ -0,0 +1,18 @@
+
+export const Rewards = () => {
+    return (<>
+
+        <div>
+            <h1>Một Con Vịt</h1>
+            <h2>500 Spicy Points</h2>
+        </div>
+
+        <div>
+            <h1>Request Any Song</h1>
+            <h2>5000 Spicy Points</h2>
+        </div>
+
+    </>
+    );
+};
+
