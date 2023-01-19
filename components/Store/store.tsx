db34169ba0873a9835b195fd0d016cdc923a374e
Thomas Nguyen <ttn@ou.edu>
2023-01-18 23:40:24 -0600

    Add message for buy tokens

--- components/Store/store.tsx
+++ components/Store/store.tsx
@@ -1,20 +1,26 @@
-import { DisplayStatus } from "../DisplayStatus/displaystatus";
+import { DisplayStatus } from '../DisplayStatus/displaystatus';
+import Popup from 'reactjs-popup';
 
 export const Store = () => {
-    return (<>
+  return (
+    <>
+      <DisplayStatus />
 
-        <DisplayStatus />
-
-
-        <div>
+      <div>
+        <Popup
+          trigger={
             <button>
-                <h1>Buy Spicy Tokens</h1>
-                <h2>5 tokens for $1</h2>
+              <h1>Buy Spicy Tokens</h1>
+              <h2>5 tokens for $1</h2>
             </button>
-
-        </div>
-
+          }
+          position="right center"
+        >
+          <div>
+            Hahahahahaha, already out of tokens? Well, you are out of luck! This feature is not available yet.
+          </div>
+        </Popup>
+      </div>
     </>
-    );
+  );
 };
-
